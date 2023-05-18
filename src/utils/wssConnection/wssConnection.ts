36e968cb7530b95a4f6a6c880f3ddd5b8c56f661
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2023-05-18 19:45:51 +0200

    first comit

---
+++ src/utils/wssConnection/wssConnection.ts
@@ -0,0 +1,13 @@
+import socketClient from 'socket.io-client'
+
+const SERVER = 'http://localhost:4000';
+
+let socket: any;
+
+export const connectWithWebSocket = ()=>{
+    socket = socketClient(SERVER);
+    socket.on('connection', ()=>{
+        console.log('succesfully connection with wss server');
+        console.log(socket.id);
+    })
+}
