b2779cc4ed476a9c1c2bc0869ed8c8ea950be27e
MariuszRudnik <86014393+MariuszRudnik@users.noreply.github.com>
2023-05-18 19:48:29 +0200

    first commit

--- src/utils/wssConnection/wssConnection.ts
+++ src/utils/wssConnection/wssConnection.ts
@@ -7,6 +7,7 @@ let socket: any;
 export const connectWithWebSocket = ()=>{
     socket = socketClient(SERVER);
     socket.on('connection', ()=>{
+
         console.log('succesfully connection with wss server');
         console.log(socket.id);
     })
